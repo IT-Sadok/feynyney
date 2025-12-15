efcd04e83a63a90d96380a350ffb9d0dac6b0ddc
Dmytro Makohonchuuk <71649645+feynyney@users.noreply.github.com>
2025-12-15 19:04:11 +0100

    Connected DB to project

---
+++ LogisticsApp/LogisticsApp/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using LogisticsApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LogisticsApp.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+        
+    }
+
+    public DbSet<User> Users => Set<User>();
+}
