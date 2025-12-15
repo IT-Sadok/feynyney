c9ad12363c7993138b669838d1adfc80376957c9
Dmytro Makohonchuuk <71649645+feynyney@users.noreply.github.com>
2025-12-15 19:04:11 +0100

    Connected DB to project

---
+++ LogisticsApp/LogisticsApp/Models/User.cs
@@ -0,0 +1,15 @@
+﻿namespace LogisticsApp.Models;
+
+public class User
+{
+    public enum UserRole
+    {
+        User,
+        Admin
+    }
+
+    public Guid UserId { get; set; } = new Guid();
+    public string UserName { get; set; }
+    public string PasswordHash { get; set; }
+    public UserRole Role { get; set; } = UserRole.User;
+}
